bcda47714f083cd4f1f950f67353e31978b0ce29
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-03 16:06:11 +0300

    Updates

---
+++ precache-manifest.7076844bca1e5230bd4d683bbc317e03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671a8f6bc86a06c8bd6e0b9216245dc2",
+    "url": "/React-Tour2-Project/index.html"
+  },
+  {
+    "revision": "2dffd7cf481eb5691c07",
+    "url": "/React-Tour2-Project/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/React-Tour2-Project/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tour2-Project/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dffd7cf481eb5691c07",
+    "url": "/React-Tour2-Project/static/js/main.9ed991ac.chunk.js"
+  },
+  {
+    "revision": "c21291abc0c64e4c500c",
+    "url": "/React-Tour2-Project/static/js/runtime-main.8e37d735.js"
+  }
+]);
